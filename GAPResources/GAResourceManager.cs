00e644f080593abde98a17a8f3e456b22339f172
AmitBansal73 <43637892+AmitBansal73@users.noreply.github.com>
2019-02-05 14:10:27 +0530

    RespurceProject

---
+++ GAPResources/GAResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAPResources
+{
+    public static class GAResourceManager
+    {
+        public static string GetResourceString(string key)
+        {
+            return GAPResources.LocalResorces.GAResource.ResourceManager.GetString(key);
+        }
+
+        public static string GetResourceString(string key, CultureInfo cultureInfo)
+        {
+            return GAPResources.LocalResorces.GAResource.ResourceManager.GetString(key, cultureInfo);
+        }
+    }
+}
